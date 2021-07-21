99d047dd62bae98662bb8df42140600518a44899
kouhon <kouichi812@gmail.com>
2021-07-21 15:19:22 +0900

    2.1

--- app/src/main/java/net/hon/kou/composerecette/MainActivity.kt
+++ app/src/main/java/net/hon/kou/composerecette/MainActivity.kt
@@ -3,11 +3,18 @@ package net.hon.kou.composerecette
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.hon.kou.composerecette.ui.theme.ComposeRecetteTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +22,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeRecetteTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                ColumnSample()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ColumnSample() {
+    Column(
+        modifier = Modifier
+            .size(300.dp)
+            .background(Color.White)
+            .scrollable(
+                state = rememberScrollState(),
+                orientation = Orientation.Vertical
+            )
+    ) {
+        Box(
+            Modifier
+                .size(100.dp)
+                .background(Color.LightGray)
+        )
+        Box(
+            Modifier
+                .size(100.dp)
+                .background(Color.Gray)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeRecetteTheme {
-        Greeting("Android")
+        ColumnSample()
     }
 }
