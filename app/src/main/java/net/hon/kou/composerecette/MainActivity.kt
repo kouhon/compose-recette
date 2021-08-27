4b11a6f60ef224ae9b5ac712097dec68efcf20fd
kouhon <kouichi812@gmail.com>
2021-08-28 08:28:37 +0900

    list-16.5

--- app/src/main/java/net/hon/kou/composerecette/MainActivity.kt
+++ app/src/main/java/net/hon/kou/composerecette/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
@@ -35,7 +36,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeRecetteTheme {
-                ModifierPadding()
+                ModifierClickable()
             }
         }
     }
@@ -45,8 +46,19 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ComposeRecetteTheme {
-        ModifierPadding()
+        ModifierClickable()
     }
+}
+
+@Composable
+fun ModifierClickable() {
+    Box(
+        modifier = Modifier
+            .size(200.dp)
+            .padding(50.dp)
+            .background(Color.Gray)
+            .clickable {  }
+    )
 }
 
 @Composable
